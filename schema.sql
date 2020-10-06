7283e8f9a7d6f556b7473d3c36987f1e86c77e61
tom2u <tbrodhead@2u.com>
2020-10-06 16:11:35 -0500

    7.2

--- schema.sql
+++ schema.sql
@@ -36,24 +36,52 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(10) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+
+-- CREATE TABLE dept_emp (
+-- 	dept_no VARCHAR(4) NOT NULL,
+-- 	emp_no INT NOT NULL,
+-- 	from_date DATE NOT NULL,
+-- 	to_date DATE NOT NULL,
+-- 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+-- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+-- 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+-- 	PRIMARY KEY (dept_no)
+-- );
+
+-- CREATE TABLE titles (
+-- 	emp_no INT NOT NULL,
+-- 	title VARCHAR(40) NOT NULL,
+-- 	from_date DATE NOT NULL,
+-- 	to_date DATE NOT NULL,
+-- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+-- 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+-- 	PRIMARY KEY (emp_no)
+-- );
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
+	title VARCHAR(50) NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+	to_date DATE,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
 
+-- Successful:
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+drop table titles;
+drop table dept_emp;
